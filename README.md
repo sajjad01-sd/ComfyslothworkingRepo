e11739a2bc2298600e6aec2f82bc86a48a06e596
sajjad01-sd <sajjadhossainsb01@gmail.com>
2021-06-28 10:51:31 +0600

    this is my first upload of this repo

---
+++ README.md
@@ -0,0 +1,26 @@
+## Notes
+
+#### Older React Version
+
+```
+"react": "^16.13.1",
+"react-dom": "^16.13.1",
+"react-scripts": "3.4.3",
+```
+
+#### Current React Version
+
+```
+"react": "^17.0.1",
+"react-dom": "^17.0.1",
+"react-scripts": "4.0.0",
+```
+
+#### Alternative fix
+
+.env file in the root
+FAST_REFRESH=FALSE
+
+// DYNAMIC URL
+I make the api url dynamic....
+helpers provide function with url and constant catch the url.
---
+++ src/utils/helpers.js
@@ -0,0 +1,18 @@
+export const formatPrice = (number) => {
+  const newNumber = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number / 100);
+
+  return newNumber;
+};
+
+export const urlDetector = () => {
+  const mainUrl = window.location.href.includes("http://localhost")
+    ? "https://comfyslothapi.herokuapp.com"
+    : window.location.href;
+  const wantedUrl = mainUrl.split(".com")[0].concat(".com");
+  return wantedUrl;
+};
+
+export const getUniqueValues = () => {};
